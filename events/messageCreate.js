eda97e01ffc7901be6132cac93518d8096c30cb3
Moses Vinson <70172435+artiumex@users.noreply.github.com>
2022-01-24 17:10:14 +0000

    poop

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'messageCreate',
 	async execute(message, client, lib) {
 		if (["499251616059359232","256880604359032832"].includes(message.author.id)){
-			message.react(💩)
+			message.react("💩");
 		}
 	},
 };

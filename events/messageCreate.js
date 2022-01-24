84801864cec0e90465aa55115b4ed461da61cb3f
Moses Vinson <70172435+artiumex@users.noreply.github.com>
2022-01-24 17:09:22 +0000

    poop

---
+++ events/messageCreate.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'messageCreate',
+	async execute(message, client, lib) {
+		if (["499251616059359232","256880604359032832"].includes(message.author.id)){
+			message.react(💩)
+		}
+	},
+};
